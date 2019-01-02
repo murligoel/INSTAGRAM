1172628584448a30dd91221a48989fe11516fea1
murligoel <murli.goel1999@gmail.com>
2019-01-02 14:40:29 +0530

    front page designed and navigation onclick to signup and login activity

---
+++ app/src/main/java/com/example/android/instagram/FrontPageActivity.java
@@ -0,0 +1,40 @@
+package com.example.android.instagram;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.android.instagram.activity.LoginActivity;
+import com.example.android.instagram.activity.SignUpActivity;
+
+public class FrontPageActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Button buttonSignUp, buttonLogIn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.front_page);
+
+
+        buttonSignUp = (Button) findViewById(R.id.buttonSignUp);
+        buttonLogIn = (Button) findViewById(R.id.buttonLogin);
+
+        buttonSignUp.setOnClickListener(this);
+        buttonLogIn.setOnClickListener(this);
+    }
+
+    public void onClick(View v){
+
+        if (v == buttonLogIn) {
+
+            startActivity(new Intent(this, LoginActivity.class));
+
+        } else if (v == buttonSignUp) {
+
+            startActivity(new Intent(this, SignUpActivity.class));
+
+        }
+    }
+}
--- app/src/main/java/com/example/android/instagram/activity/LoginActivity.java
+++ app/src/main/java/com/example/android/instagram/activity/LoginActivity.java
@@ -1,13 +1,15 @@
-package com.example.android.instagram;
+package com.example.android.instagram.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.android.instagram.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
