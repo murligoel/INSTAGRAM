d271ded16568cb46633973fc3f6b226e86c5bbb4
murligoel <murli.goel1999@gmail.com>
2019-01-02 23:36:20 +0530

    implemented user interface for login and signup

--- app/src/main/java/com/example/android/instagram/activity/SignUpActivity.java
+++ app/src/main/java/com/example/android/instagram/activity/SignUpActivity.java
@@ -1,15 +1,34 @@
 package com.example.android.instagram.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.android.instagram.R;
 
-public class SignUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        login = (TextView) findViewById(R.id.go_to_login);
+
+        login.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v == login) {
+
+            startActivity(new Intent(this, LoginActivity.class));
+        }
     }
 }
