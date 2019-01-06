39016d8855ee5b490fcaf0c831eae7a7efb62d9c
murligoel <murli.goel1999@gmail.com>
2019-01-06 18:37:09 +0530

    added fragments and other layouts and non Swipeable View pager

---
+++ app/src/main/java/com/example/android/instagram/activity/FilterActivity.java
@@ -0,0 +1,14 @@
+package com.example.android.instagram.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.android.instagram.R;
+
+public class FilterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+    }
+}
---
+++ app/src/main/java/com/example/android/instagram/fragments/EditImageFragment.java
@@ -0,0 +1,31 @@
+package com.example.android.instagram.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.instagram.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EditImageFragment extends Fragment {
+
+
+    public EditImageFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_edit_image, container, false);
+    }
+
+}
